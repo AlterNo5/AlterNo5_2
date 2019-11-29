c5ebc4656ad91835074910400249a1e294f710b1
AlterNo5 <56732760+AlterNo5@users.noreply.github.com>
2019-11-29 17:08:51 -0600

    Boss3, 60% Completado

---
+++ AlterNo.5_2/Assets/Scripts/BallBoss.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.AI;
+
+public enum BossBallStates
+{
+    Idle = 0,
+    Move = 1,
+    Follow = 2,
+    Attack = 3
+}
+
+public class BossBall : MonoBehaviour
+{
+    private FSM m_myStateMachine = new FSM();
+
+    private NavMeshAgent m_agent;
+
+    public GameObject m_target;
+
+    public Animator m_anim;
+    private Vector3 m_destinatedMove;
+    private Vector3 m_actualPosition;
+    private float m_countChange = 0f;
+    private float m_timeToChange;
+    private bool m_inAttackRange;
+    private bool focus = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_myStateMachine.InitStateMachine(EnterIdle, UpdateIdle, (int)BossBallStates.Idle);
+        m_agent = GetComponent<NavMeshAgent>();
+        m_anim = this.gameObject.transform.GetChild(0).GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_myStateMachine.UpdateStateMachine();
+        if (m_myStateMachine.GetCurrentState == (int)BossBallStates.Move
+            || m_myStateMachine.GetCurrentState == (int)BossBallStates.Follow)
+        {
+            float value = m_agent.velocity.sqrMagnitude;
+            m_anim.SetFloat("Movement", value);
+        }
+        else
+        {
+            m_anim.SetFloat("Movement", 0);
+        }
+        
+
+    }
+
+    private void ChangeState(BossBallStates state)
+    {
+        if (state != (BossBallStates)m_myStateMachine.GetCurrentState)
+        {
+            switch (state)
+            {
+                case BossBallStates.Idle:
+                    ChangeNow(EnterIdle, UpdateIdle, state);
+                    break;
+                case BossBallStates.Move:
+                    ChangeNow(EnterMove, UpdateMove, state);
+                    break;
+                case BossBallStates.Follow:
+                    ChangeNow(EnterFollow, UpdateFollow, state);
+                    break;
+                case BossBallStates.Attack:
+                    ChangeNow(EnterAttack, UpdateAttack, state);
+                    break;
+            }
+        }
+    }
+
+    public void ChangeNow(Action enter, Action update, BossBallStates state)
+    {
+        switch ((BossBallStates)m_myStateMachine.GetCurrentState)
+        {
+            case BossBallStates.Idle:
+                m_myStateMachine.ChangeState(enter, update, ExitIdle, (int)state);
+                break;
+            case BossBallStates.Move:
+                m_myStateMachine.ChangeState(enter, update, ExitMove, (int)state);
+                break;
+            case BossBallStates.Follow:
+                m_myStateMachine.ChangeState(enter, update, ExitFollow, (int)state);
+                break;
+            case BossBallStates.Attack:
+                m_myStateMachine.ChangeState(enter, update, ExitAttack, (int)state);
+                break;
+        }
+    }
+
+
+
+    public void SetTarget(BossBall v)
+    {
+   
+    }
+
+    public void AttackRange(bool b)
+    {
+        m_inAttackRange = b;
+    }
+
+    #region Idle
+
+    private void EnterIdle()
+    {
+       
+
+    }
+    private void UpdateIdle()
+    {
+
+    }
+   
+
+
+
+
+    private void ExitIdle()
+    {
+       
+    }
+    #endregion
+
+    #region Move
+    private void EnterMove()
+    {
+      
+
+    }
+    private void UpdateMove()
+    {
+    
+    }
+
+        private void ExitMove()
+    {
+     
+
+    }
+    #endregion
+
+    #region Follow
+    private void EnterFollow()
+    {
+        
+           
+       
+    }
+    private void UpdateFollow()
+    {
+
+       
+    }
+
+    private void ExitFollow()
+    {
+      
+    }
+    #endregion
+
+    #region Attack
+    private void EnterAttack()
+    {
+        
+    }
+    private void UpdateAttack()
+    {
+       
+
+    }
+
+    private void ExitAttack()
+    {
+        
+        
+    }
+    #endregion
+}
---
+++ AlterNo.5_2/Assets/Scripts/FSM.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class FSM
+{
+    private Action m_enterState;
+    private Action m_updateState;
+    private Action m_exitState;
+
+    private int m_desiredState = -1;
+    private int m_currentState = -1;
+
+    public int GetCurrentState
+    {
+        get { return m_currentState; }
+    }
+
+    public void InitStateMachine(Action enter, Action update, int state)
+    {
+        m_enterState = enter;
+        m_updateState = update;
+        m_desiredState = state;
+    }
+
+    public void UpdateStateMachine()
+    {
+        if (m_desiredState == -1) { return; }
+
+        if (m_desiredState != m_currentState)
+        {
+            m_exitState?.Invoke();
+            m_enterState?.Invoke();
+            m_currentState = m_desiredState;
+        }
+        else
+        {
+            m_updateState?.Invoke();
+        }
+
+    }
+
+    public void ChangeState(Action enter, Action update, Action oldexit, int state)
+    {
+        m_enterState = enter;
+        m_updateState = update;
+        m_exitState = oldexit;
+        m_desiredState = state;
+    }
+
+
+}
