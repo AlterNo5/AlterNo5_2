e3042a5a237b069da75bb89c3ab17ed41d56d4a7
AlterNo5 <56732760+AlterNo5@users.noreply.github.com>
2019-12-02 06:39:02 -0600

    Acomodo de props Nivel3

--- AlterNo.5_2/Assets/Scripts/Objetos/MoveObjects.cs
+++ AlterNo.5_2/Assets/Scripts/Objetos/MoveObjects.cs
@@ -4,12 +4,36 @@ using UnityEngine;
 
 public class MoveObjects : MonoBehaviour
 {
-  
-    
 
-   
-    void Update()
+    public bool rotationy;
+    public bool jump;
+    private Vector3 startPosition;
+    public float rotationSpeed;
+    public float moveSpeed;
+    private int direction;
+
+    private void Start()
     {
-        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+        startPosition = transform.position;
+        direction = 1;
+
+    }
+    void FixedUpdate()
+    {
+        if (rotationy)
+        {
+            transform.Rotate(new Vector3(0, 1, 0) * (rotationSpeed * Time.deltaTime));
+        }
+        if (jump)
+        {
+            if (transform.position.y >= startPosition.y+0.5f || transform.position.y <= startPosition.y - 0.5f)
+            {
+                direction *= -1;
+            }
+
+            transform.Translate(new Vector3(0, direction, 0) * (moveSpeed * Time.deltaTime));
+
+        }
+        
     }
 }
